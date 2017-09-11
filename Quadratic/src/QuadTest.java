8fac0faa941cfd60143b05570bcbaa9dcb7d87c4
rileyp2000 <rileyp@bishopireton.org>
2017-09-10 23:31:22 -0400

    added test class

---
+++ Quadratic/src/QuadTest.java
@@ -0,0 +1,23 @@
+/**
+ *
+ *<p></p>
+ *@author Patrick Riley
+ *2017-09-10
+ *
+ */
+
+/**
+ * @author Patrick
+ *
+ */
+public class QuadTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
